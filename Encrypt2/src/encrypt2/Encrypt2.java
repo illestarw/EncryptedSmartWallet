daccaaad7d840a284078f6c75c34b8bcc7d9cefe
Neil Prakasam <neilprakasam@gmail.com>
2018-10-24 17:42:57 -0700

    AES Encrypt
    need to encrypt bytes in another data structure than string

---
+++ Encrypt2/src/encrypt2/Encrypt2.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package encrypt2;
+
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.BadPaddingException;
+
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+//import org.javatuples.KeyValue; 
+
+
+
+/**
+ *
+ * @author neilp
+ */
+public class Encrypt2 {
+
+    private static byte[] key;
+    private static SecretKeySpec secretKey;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        // TODO code application logic here
+        int senderID=1111, receiverID=2222,amount=100,counter=1;
+        
+        String AESKey="752EF0D8FB4958670DBA40AB1F3C1D0F8FB4958670DBA40AB1F3752EF0DC1D0F";
+            KeyGenerator generator;
+            
+        try {
+            generator = KeyGenerator.getInstance("AES");
+            generator.init(256);
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(Encrypt2.class.getName()).log(Level.SEVERE, null, ex);
+            
+        }
+        
+             // The AES key size in number of bits
+
+            setKey(AESKey);
+            String plainText = "1111222201000001";
+            Cipher aesCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            aesCipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            byte[] byteCipherText = aesCipher.doFinal(plainText.getBytes());
+            
+            System.out.println(Arrays.toString(byteCipherText));
+
+    }
+
+    private static void setKey(String password) {
+          MessageDigest sha = null;
+        try {
+            key = password.getBytes("UTF-8");
+            sha = MessageDigest.getInstance("SHA-256");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16);
+            secretKey = new SecretKeySpec(key, "AES");
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+    }
+    
+}
